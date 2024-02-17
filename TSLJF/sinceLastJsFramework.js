d40475cdf488a5d59065fdb4ec4568d3f44912a3
Salvadego Matheus <matheus.salvadego@escola.pr.gov.br>
2024-02-17 16:02:59 -0300

    Added a not so accurate Record Score

--- TSLJF/sinceLastJsFramework.js
+++ TSLJF/sinceLastJsFramework.js
@@ -1,12 +1,15 @@
-const times = document.getElementById("frameworks");
+const currentRecord = document.getElementById("record");
+const currentRecordDate = document.getElementById("dateOfRecord");
 
+const times = document.getElementById("frameworks");
 const hr = document.getElementById("hr");
 const min = document.getElementById("min");
 const sec = document.getElementById("secs");
 
 async function getPackages() {
   const response = await fetch(
-    "https://registry.npmjs.com/-/v1/search?text=framework&size=250",
+    "https://registry.npmjs.com/-/v1/search?text=framework&size=25000",
+    //250 is max size unfortunately, which means that the record is not exactly accurate
   );
   const data = await response.json();
   const { objects } = data;
@@ -14,15 +17,38 @@ async function getPackages() {
   return objects;
 }
 
-async function getPackagesCreatedToday() {}
+async function getRecordOfMostPackagesCreated() {
+  const packages = await getPackages();
+  const packagesByDay = {};
+
+  packages.forEach((pkg) => {
+    const createdAt = new Date(pkg.package.date);
+    const day = createdAt.toISOString().split("T")[0];
+    packagesByDay[day] = (packagesByDay[day] || 0) + 1;
+  });
+
+  let record = 0;
+  let recordDate = "";
+
+  for (const day in packagesByDay) {
+    if (packagesByDay[day] > record) {
+      record = packagesByDay[day];
+      recordDate = day;
+    }
+  }
+
+  currentRecord.innerText = `${record} frameworks `;
+  currentRecordDate.innerText = `${recordDate}`;
+}
 
 async function logPackageCount() {
-  const objects = await getPackages();
+  const packages = await getPackages();
+
   const today = new Date();
   today.setHours(0, 0, 0, 0);
 
-  const packagesCreatedToday = objects.filter((pkg) => {
-    const pkgDate = new Date(pkg.package.date);
+  const packagesCreatedToday = packages.filter((pkg) => {
+    const pkgDate = getPackegeDate(pkg);
     return pkgDate.setHours(0, 0, 0, 0) === today.getTime();
   });
 
@@ -30,15 +56,19 @@ async function logPackageCount() {
   times.innerText = count;
 }
 
+function getPackegeDate(pkg) {
+  return new Date(pkg.package.date);
+}
+
 async function logTime() {
-  const objects = await getPackages();
-  const [first] = objects.sort((a, b) =>
+  const packages = await getPackages();
+  const [first] = packages.sort((a, b) =>
     b.package.date.localeCompare(a.package.date),
   );
-  const date = new Date(first.package.date);
-  const since = Date.now() - date.getTime();
 
-  formatTime(since / 1000);
+  const pkgDate = getPackegeDate(first);
+  const sinceInSeconds = (Date.now() - pkgDate.getTime()) / 1000;
+  formatTime(sinceInSeconds);
 }
 
 function formatTime(seconds) {
@@ -46,15 +76,17 @@ function formatTime(seconds) {
   const minutes = Math.floor((seconds % 3600) / 60);
   const secs = (seconds % 60).toFixed(0);
 
-  paddedHours = hours >= 10 ? hours.toString() : "0" + hours.toString();
-  paddedMinutes = minutes >= 10 ? minutes.toString() : "0" + minutes.toString();
-  paddedSeconds = secs >= 10 ? secs.toString() : "0" + secs.toString();
-
-  hr.innerText = paddedHours;
-  min.innerText = paddedMinutes;
-  sec.innerText = paddedSeconds;
+  hr.innerText = toPaddedString(hours);
+  min.innerText = toPaddedString(minutes);
+  sec.innerText = toPaddedString(secs);
 }
 
+function toPaddedString(time) {
+  return time >= 10 ? time.toString() : "0" + time.toString();
+}
+
+getRecordOfMostPackagesCreated();
+logTime();
 logPackageCount();
 
 setInterval(() => {
